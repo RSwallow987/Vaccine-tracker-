afb0a3f3fa73ff4092156c7cfa967b3232829e39
Rachel Swallow <rswallow987@gmail.com>
2021-05-04 09:18:42 +0200

    Morning work

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const router = express.Router();
+
+//serve static files in express
+app.use(express.static(path.join(__dirname, "public"))); //then e.g. this will work http://localhost:3000/images/firefox-icon.png
+
+router.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/views/index.html"));
+  //__dirname : It will resolve to your project folder.
+});
+
+router.get("/about", function (req, res) {
+  res.sendFile(path.join(__dirname + "/views/about.html"));
+});
+
+//add the router
+app.use("/", router);
+app.listen(process.env.port || 3000);
+
+console.log("Running at Port 3000");
